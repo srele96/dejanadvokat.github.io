10513ac582222514d166eee25e33414eaec7a8a3
Srecko <srele96@gmail.com>
2025-10-29 00:07:24 +0100

    use site url config & extract jekyll_env

--- build-site.sh
+++ build-site.sh
@@ -3,7 +3,16 @@
 set -euo pipefail
 # ^ Ensures script stops immediately if any command fails
 
-echo "🚀 Starting production build..."
+echo "🔍 Checking required environment variables..."
+
+if [[ -z "${JEKYLL_ENV:-}" ]]; then
+  echo "❌ Error: JEKYLL_ENV is not set."
+  echo "   Please run the script like:"
+  echo "     JEKYLL_ENV=<SOME_VALUE> ./build-site.sh"
+  exit 1
+fi
+
+echo "🚀 Starting build..."
 
 echo "📦 Building JavaScript"
 node ./build.prod.js
@@ -12,17 +21,16 @@ echo "✅ JavaScript build completed."
 
 echo "🧱 Building Jekyll site..."
 
-JEKYLL_ENV=production
 JEKYLL_CMD=(bundle exec jekyll build)
 
-# only add --baseurl if BASEURL exists and is not empty
-if [[ -n "${BASEURL:-}" ]]; then
-  echo "🔧 Using baseurl: $BASEURL"
-  JEKYLL_CMD+=(--baseurl "$BASEURL")
+# only add --baseurl if SITE_BASEURL exists and is not empty
+if [[ -n "${SITE_BASEURL:-}" ]]; then
+  echo "🔧 Using baseurl: $SITE_BASEURL"
+  JEKYLL_CMD+=(--baseurl "$SITE_BASEURL")
 else
-  echo "⚙️ No baseurl set, building normally."
+  echo "⚙️ No SITE_BASEURL set, building normally."
 fi
 
-JEKYLL_ENV=production "${JEKYLL_CMD[@]}"
+"${JEKYLL_CMD[@]}"
 
 echo "✅ Jekyll site built successfully."
